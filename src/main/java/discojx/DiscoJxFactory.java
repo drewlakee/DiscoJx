5de1f99f82a6b0671763a3d5148b7812099be69d
Andrew Aleynikov <drew.lake@yandex.ru>
2021-05-04 16:56:47 +0300

    remove static methods from api factory

--- src/main/java/discojx/DiscoJxFactory.java
+++ src/main/java/discojx/DiscoJxFactory.java
@@ -1,28 +1,8 @@
 package discojx;
 
-import discojx.clients.authentication.PersonalAccessToken;
 import discojx.discogs.api.DiscogsApi;
-import org.apache.http.Header;
-
-import java.util.List;
 
 public interface DiscoJxFactory {
 
     DiscogsApi create();
-
-    static DiscogsApi createDefaultApi() {
-        return new DefaultApiDiscoJxFactory().create();
-    }
-
-    static DiscogsApi createDefaultApi(List<Header> defaultHeaders) {
-        return new DefaultApiDiscoJxFactory().setDefaultHttpClientHeaders(defaultHeaders).create();
-    }
-
-    static DiscogsApi createDefaultApiWithToken(PersonalAccessToken token) {
-        return new DefaultApiWithTokenDiscoJxFactory(token).create();
-    }
-
-    static DiscogsApi createDefaultApiWithToken(PersonalAccessToken token, List<Header> defaultHeaders) {
-        return new DefaultApiWithTokenDiscoJxFactory(token).setDefaultHttpClientHeaders(defaultHeaders).create();
-    }
 }
